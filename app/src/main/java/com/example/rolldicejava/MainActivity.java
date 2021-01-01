741d1daea1ac4829b16103cf97463653436d7cab
sahilkumardev <kumar.sahil4512@gmail.com>
2021-01-01 03:04:14 -0500

    N/A

--- app/src/main/java/com/example/rolldicejava/MainActivity.java
+++ app/src/main/java/com/example/rolldicejava/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.rolldicejava;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.ImageReader;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+
 
 import java.util.Random;
 
